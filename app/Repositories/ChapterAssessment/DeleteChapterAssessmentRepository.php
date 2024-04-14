531d847c326e496022ec3de4038b48be01bc51a5
John Binze Escol <escoljohnbinze@gmail.com>
2024-04-14 10:39:43 +0800

    Refactor delete methods in DeleteChapterAssessmentRepository and DeleteExamRepository

--- app/Repositories/ChapterAssessment/DeleteChapterAssessmentRepository.php
+++ app/Repositories/ChapterAssessment/DeleteChapterAssessmentRepository.php
@@ -11,9 +11,19 @@ class DeleteChapterAssessmentRepository extends BaseRepository
     public function execute($referenceNumber)
     {
         if ($this->user()->hasRole('ADMIN')) {
-            $chap_ass = ChapterAssessment::where('reference_number', $referenceNumber)->firstOrFail();
-            $chap_ass->delete();
-            return $this->success("Chapter Assessment successfully deleted.");
+            $chap_ass = ChapterAssessment::whereHas('chapter', function ($query) use ($referenceNumber) {
+                $query->where('reference_number', $referenceNumber);
+            })->get();
+
+            if ($chap_ass->isEmpty()) {
+                return $this->error("No Chapter Assessment found for the specified chapter.");
+            }
+
+            foreach ($chap_ass as $ca) {
+                $ca->delete();
+            }
+
+            return $this->success("Chapter Assessment for the specified chapter successfully deleted.");
         } else {
             return $this->error("You are not authorized to delete a chapter assessment.");
         }
--- app/Repositories/Exam/DeleteExamRepository.php
+++ app/Repositories/Exam/DeleteExamRepository.php
@@ -8,12 +8,17 @@ use App\Models\Exam;
 
 class DeleteExamRepository extends BaseRepository
 {
-    public function execute($referenceNumber){
+    public function execute($referenceNumber)
+    {
         if ($this->user()->hasRole('ADMIN')) {
             $exams = Exam::whereHas('chapter', function ($query) use ($referenceNumber) {
                 $query->where('reference_number', $referenceNumber);
             })->get();
 
+            if ($exams->isEmpty()) {
+                return $this->error("No exams found for the specified chapter.");
+            }
+            
             foreach ($exams as $exam) {
                 $exam->delete();
             }
