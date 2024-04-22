59bc529a0b09bd9b9fa01c7edec1792781715958
John Binze Escol <escoljohnbinze@gmail.com>
2024-04-22 11:05:06 +0800

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'http://localhost:80', 'http://localhost:8080'],
 
     'allowed_origins_patterns' => ['*'],
 
